40a0c6fff218f00ae1906c2d2b416000b31cb39b
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-07-31 19:41:17 -0600

    Implemented docs disabling when server is on prod. Created CORS middleware for proper server communication

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 # In-built dependencies
 from typing import Union
+import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
-from app.routers import (auth_router, legal_case_router, invoice_router)
+from app.routers import (auth_router, legal_case_router, invoice_router, client_router)
 
+load_dotenv()
 
-app = FastAPI()
-# TODO: Generate and add app's middleware
+# Origins for CORS
+origins = [
+    "http://localhost:3000",
+    "http://ec2-78-13-106-228.mx-central-1.compute.amazonaws.com:3000"
+    "https://ec2-78-13-106-228.mx-central-1.compute.amazonaws.com:3000"
+    ]
+
+environment = os.getenv("ENVIRONMENT")
+if environment == "production":
+    app = FastAPI(docs_url=None, redoc_url=None)
+else:
+    app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 
 @app.get("/")
