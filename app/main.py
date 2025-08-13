45aa36857423e09e4453af76983233ba73b2e94f
Jerh21 <hamjosue27@gmail.com>
2025-08-13 15:28:55 -0600

    Agregados los endpoints para editar factura y ver perfil

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
-from app.routers import (auth_router, legal_case_router, invoice_router, notification_router)
+from app.routers import (auth_router, legal_case_router, invoice_router, notification_router, user_router)
 
 load_dotenv()
 
@@ -42,6 +42,6 @@ app.include_router(auth_router.router)
 app.include_router(legal_case_router.router)
 app.include_router(invoice_router.router)
 app.include_router(notification_router.router)
-            
+app.include_router(user_router.router)
 
 
