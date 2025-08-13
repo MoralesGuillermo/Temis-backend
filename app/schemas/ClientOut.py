45c25794319dc1ffec70693147cdbdb61b74f7e8
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-08-13 13:37:11 -0600

    Revert "Merge pull request #7 from MoralesGuillermo/feature-get-case-files"
    This reverts commit 2fd2522bfb805be44f679437c6471619fdf3c8ba, reversing
    changes made to 83428160c46afa77531c83f3282e89781b81d966.

--- app/schemas/ClientOut.py
+++ app/schemas/ClientOut.py
@@ -1,10 +1,10 @@
 """Modelo para retornar un LegalCase al Front"""
 from typing import Annotated, Optional
-from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+from pydantic import BaseModel, Field
 
 
 class ClientOut(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
     id: Annotated[int, Field(gt=0)]
     first_name: Annotated[str, Field(min_length=1, max_length=255)]
     last_name: Annotated[str, Field(min_length=1, max_length=255)]
--- app/schemas/LegalCaseOut.py
+++ app/schemas/LegalCaseOut.py
@@ -1,16 +1,14 @@
 """Modelo para retornar un LegalCase al Front"""
 from typing import Annotated, Optional
 from datetime import datetime
-from pydantic import BaseModel, Field, ConfigDict
-from app.schemas.ClientOut import ClientOut
+from pydantic import BaseModel, Field
 
 
 class LegalCaseOut(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
     id: Annotated[int, Field(gt=0)]
     start_date: datetime
     case_type: str
-    client: ClientOut
+    client_id: Annotated[int, Field(gt=0)]
     description: Annotated[str, Field(min_length=1, max_length=255)]
     notes: str
     title: Annotated[str, Field(min_length=1, max_length=100)]
@@ -18,5 +16,8 @@ class LegalCaseOut(BaseModel):
     end_date: Optional[datetime]
     priority_level: str
     status: str
+
+    class Config:
+        from_attributes = True
 
     
